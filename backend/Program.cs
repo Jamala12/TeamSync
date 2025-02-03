df3cf446fe2ccebb3c8a4cb7702cab906b89106a
Jamal <jamal@Jamals-MacBook-Air.local>
2025-02-03 17:14:31 +0000

    update, back on project

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello, World!");
---
+++ backend/configurations/MongoDBSettings.cs
@@ -0,0 +1,8 @@
+namespace TeamSync.Configurations
+{
+    public class MongoDBSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
